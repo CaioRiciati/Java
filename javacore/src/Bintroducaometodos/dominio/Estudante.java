79450643aa181e7761be2091a13fd60052d3dc0f
Caio Riciati <esdrascaio5313@gmail.com>
2025-03-12 21:19:49 -0300

    ta indo

---
+++ javacore/src/Bintroducaometodos/dominio/Estudante.java
@@ -0,0 +1,17 @@
+package Bintroducaometodos.dominio;
+
+public class Estudante {
+    public String nome;
+    public int idade;
+    public char genero;
+    public int RA;
+
+    public void imprime(){
+        System.out.println("-----------------");
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+        System.out.println(this.genero);
+    }
+
+}
+
---
+++ javacore/src/Bintroducaometodos/test/EstudanteTest02.java
@@ -0,0 +1,26 @@
+   package Bintroducaometodos.test;
+   import Bintroducaometodos.dominio.*;
+
+
+
+public class EstudanteTest02 {
+    public static void main(String[] args) {
+        Estudante estudante01 = new Estudante();
+        Estudante estudante02 = new Estudante();
+        Estudante estudante3 = new Estudante();
+
+        estudante02.nome = "NARUTO";
+        estudante02.idade = 21;
+        estudante02.genero = 'M';
+
+        estudante01.nome = "kakuzu";
+        estudante01.idade = 31;
+        estudante01.genero = 'M';
+
+        estudante3.nome = "sasuke";
+        estudante3.idade = 16;
+        estudante3.genero = 'M';
+
+        estudante.imprime();
+    }
+}
