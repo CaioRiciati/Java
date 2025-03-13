e6025befe6d1236d5bee574e36f6d05dfcfedf7e
Caio Riciati <esdrascaio5313@gmail.com>
2025-03-12 21:27:26 -0300

    EstudanteTest02 foi

--- javacore/src/Bintroducaometodos/test/EstudanteTest02.java
+++ javacore/src/Bintroducaometodos/test/EstudanteTest02.java
@@ -9,6 +9,7 @@ public class EstudanteTest02 {
         Estudante estudante02 = new Estudante();
         Estudante estudante3 = new Estudante();
 
+
         estudante02.nome = "NARUTO";
         estudante02.idade = 21;
         estudante02.genero = 'M';
@@ -16,11 +17,15 @@ public class EstudanteTest02 {
         estudante01.nome = "kakuzu";
         estudante01.idade = 31;
         estudante01.genero = 'M';
+        estudante01.RA = 128823001;
 
         estudante3.nome = "sasuke";
         estudante3.idade = 16;
         estudante3.genero = 'M';
 
-        estudante.imprime();
+        estudante01.imprime();
+        estudante01.imprime();
+        estudante01.imprime();
+        estudante01.imprime();
     }
 }
