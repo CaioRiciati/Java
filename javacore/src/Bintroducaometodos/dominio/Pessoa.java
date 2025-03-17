a7ae845ff2b64dc16bf26299a7ae867deb4aff1b
Caio Riciati <esdrascaio5313@gmail.com>
2025-03-16 21:29:38 -0300

    getters e setters, e privates tmb

--- javacore/src/Bintroducaometodos/dominio/Pessoa.java
+++ javacore/src/Bintroducaometodos/dominio/Pessoa.java
@@ -8,12 +8,22 @@ public class Pessoa {
         System.out.print(this.nome + " " + this.idade);
 
     }
-    public void setNome(String nome){
-        this.nome = nome;
+    public void setNome(String x){
+        nome = x;
     }
 
     public void setIdade(int idade){
-        this.idade = idade;
-    }
+        if (idade <= 0) {
+            System.out.print("idade invalida!! ");
+            return;
+        }
+            this.idade = idade;
 
+        }
+        public String getNome(){
+            return this.nome.toUpperCase();
+    }
+    public int getIdade(){
+        return this.idade;
+    }
 }
--- javacore/src/Bintroducaometodos/test/PessoaTest01.java
+++ javacore/src/Bintroducaometodos/test/PessoaTest01.java
@@ -6,9 +6,12 @@ public class PessoaTest01 {
     public static void main(String[] args) {
         Pessoa pessoa = new Pessoa();
         pessoa.setNome("caio");
-        pessoa.setIdade(17);
+        pessoa.setIdade(-3);
 
         pessoa.imprime();
+
+        System.out.print("\n"+pessoa.getNome());
+        System.out.print(pessoa.getIdade());
 
     }
 
