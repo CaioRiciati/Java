91497dca452c8ded44a9ac4c96e221dbce4d48de
Caio Riciati <esdrascaio5313@gmail.com>
2025-04-01 22:57:25 -0300

    data e local em java

--- POO/src/data_hora/data.java
+++ POO/src/data_hora/data.java
@@ -1,5 +1,26 @@
 package data_hora;
 
-public class data {
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
+public class data {
+	public static void main(String[] args) {
+		LocalDate data = LocalDate.now();
+		LocalDateTime data2 = LocalDateTime.now();
+		Instant data3 = Instant.now();
+		
+		LocalDate data4 = LocalDate.parse("2022-02-03");
+		LocalDateTime data5 = LocalDateTime.parse("2022-02-03T01:30:23");
+		Instant data6 = Instant.parse("2022-02-03T01:30:23Z");
+		Instant data7 = Instant.parse("2022-02-03T01:30:23+03:00");
+		
+		System.out.println(data);
+		System.out.println(data2);
+		System.out.println(data3);
+		System.out.println(data4);
+		System.out.println(data5);
+		System.out.println(data6);
+		System.out.println(data7);
+	}
 }
