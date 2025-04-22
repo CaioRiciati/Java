6b36469730985a90be650dfe99bbdfc0c5b7971f
Caio Riciati <esdrascaio5313@gmail.com>
2025-04-21 23:11:53 -0300

    a

--- Enumeraçoes/src/entities/HourContract.java
+++ Enumeraçoes/src/entities/HourContract.java
@@ -42,6 +42,8 @@ public class HourContract {
 		Hours = hours;
 	}
 	
-	
+	public double totalValue() {
+		return valuePerHour * Hours;
+	}
 	
 }
--- Polimorfismo/src/entities/ImportedProduct.java
+++ Polimorfismo/src/entities/ImportedProduct.java
@@ -1,5 +1,28 @@
 package entities;
 
-public class ImportedProduct {
+public final class ImportedProduct extends Product{
+	private Double customFee;
+	
+	public ImportedProduct() {
+		
+	}
+
+	public ImportedProduct(String name, Double price, Double customFee) {
+		super(name, price);
+		this.customFee = customFee;
+	}
+
+	public Double getCustomFee() {
+		return customFee;
+	}
+
+	public void setCustomFee(Double customFee) {
+		this.customFee = customFee;
+	}
+	
+	@Override
+	public String priceTag() {
+		return super.priceTag() + " (Custom fee: 20.00)";
+	}
 
 }
