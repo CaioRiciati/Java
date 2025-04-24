d9aec42cb5790f03d06b2712707d86d92129fb21
Caio Riciati <esdrascaio5313@gmail.com>
2025-04-23 22:22:17 -0300

    execoes

--- Exceções/src/application/Program.java
+++ Exceções/src/application/Program.java
@@ -5,19 +5,41 @@ import java.util.Scanner;
 
 public class Program {
 	public static void main(String [] args) {
-			Scanner sc = new Scanner(System.in);
-			try {
-			String[] vect = sc.nextLine().split(" ");
-			int position = sc.nextInt();
-			System.out.println(vect[position]);
-			}catch(InputMismatchException a) {
-				System.out.println("Invalid charactere");
-			}
-			catch(ArrayIndexOutOfBoundsException b){
-				System.out.println("Invalid number");
-			}
-			
-			System.out.println("End of program...");
-			sc.close();
+		
+		
+		method1();
+		System.out.println("End of program...");
 	}	
+	
+	
+	public static void method1() {
+		System.out.println("**Method1 start**");
+		method2();
+		
+		System.out.println("**Metjod1 End**");
+	}
+	
+	
+	public static void method2() {
+		
+		System.out.println("**Method2 start**");
+		Scanner sc = new Scanner(System.in);
+		try {
+		String[] vect = sc.nextLine().split(" ");
+		int position = sc.nextInt();
+		System.out.println(vect[position]);
+		}catch(InputMismatchException a) {
+			System.out.println("Invalid charactere");
+		}
+		catch(ArrayIndexOutOfBoundsException b){
+			System.out.println("Invalid number");
+			
+			b.printStackTrace();
+		}
+		
+		System.out.println("**Metjod2 End**");
+		
+
+		sc.close();
+	}
 }
