a2dde53fd969aea6fa05a40ba507677fb18cb2c3
Caio Riciati <esdrascaio5313@gmail.com>
2025-04-24 22:25:32 -0300

    Exceçoes acabou

--- Exceções/src/application/Program.java
+++ Exceções/src/application/Program.java
@@ -5,41 +5,30 @@ import java.util.Scanner;
 
 public class Program {
 	public static void main(String [] args) {
-		
-		
-		method1();
-		System.out.println("End of program...");
-	}	
-	
-	
-	public static void method1() {
-		System.out.println("**Method1 start**");
-		method2();
-		
-		System.out.println("**Metjod1 End**");
-	}
-	
-	
-	public static void method2() {
-		
-		System.out.println("**Method2 start**");
 		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter account data: ");
+		
 		try {
-		String[] vect = sc.nextLine().split(" ");
-		int position = sc.nextInt();
-		System.out.println(vect[position]);
-		}catch(InputMismatchException a) {
-			System.out.println("Invalid charactere");
-		}
-		catch(ArrayIndexOutOfBoundsException b){
-			System.out.println("Invalid number");
-			
-			b.printStackTrace();
+		System.out.print("Number: ");
+		Integer number= sc.nextInt();
+		sc.next();
+		System.out.print("Holder: ");
+		String Holder = sc.nextLine();
+		
+		System.out.print("Initial Balance: ");
+		Double balance =sc.nextDouble();
+		sc.next();
+		
+		System.out.print("Withdraw Limit: ");
+		Double withdrawLimit =sc.nextDouble();
+		} catch (MatchException e) {
+			// TODO: handle exception
 		}
 		
-		System.out.println("**Metjod2 End**");
 		
-
+		
+		
 		sc.close();
 	}
 }
